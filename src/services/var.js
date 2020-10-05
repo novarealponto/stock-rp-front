8d8270b8e80c2384935b516c1f076c32a939dff5
guilhermestain <guilhermestain@gmail.com>
2020-10-05 17:44:01 -0300

    var

--- src/services/var.js
+++ src/services/var.js
@@ -2,11 +2,7 @@
 
 // export const BACKEND_URL = "http://192.168.90.244:5312";
 
-<<<<<<< HEAD
 // export const BACKEND_URL = "http://localhost:5312";
-=======
-export const BACKEND_URL = "http://192.168.90.201:5312";
->>>>>>> add logout and update pass drawer externo
 
 export const BACKEND_URL = "http://192.168.90.200:5312";
 
