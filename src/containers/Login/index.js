0bbfc7aab4f1958f666df34a9fbf4aceeb22c240
jessiLeandro <jessi_leandro@hotmail.com>
2020-12-04 11:24:32 -0300

    chore : fix indentation and remove unutilized function

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -8,43 +8,38 @@ const { Password } = Input;
 const { Item: ItemForm } = Form;
 
 const roleRequired = (message) => {
-  return{
+  return {
     required: true,
     message,
-  }
-}
+  };
+};
 
 const LoginContainer = ({ onSubmit }) => {
   return (
     <div className={styles.container}>
-        <h1>Estoque - RP</h1>
-        <img src="../../retina.png" alt="company logo" />
-        <Form name="signIn" onFinish={onSubmit} onFinishFailed={() => {}}>
-          <ItemForm
-            name="username"
-            rules={[roleRequired('insira seu username')]}
-          >
-            <Input
-              placeholder="Digite seu username"
-              prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-            />
-          </ItemForm>
+      <h1>Estoque - RP</h1>
+      <img src="../../retina.png" alt="company logo" />
+      <Form name="signIn" onFinish={onSubmit}>
+        <ItemForm name="username" rules={[roleRequired('insira seu username')]}>
+          <Input
+            placeholder="Digite seu username"
+            prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+          />
+        </ItemForm>
 
-          <ItemForm name="password"
-            rules={[roleRequired('insira sua senha')]}
-            >
-            <Password
-              placeholder="Digite a senha"
-              prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-            />
-          </ItemForm>
+        <ItemForm name="password" rules={[roleRequired('insira sua senha')]}>
+          <Password
+            placeholder="Digite a senha"
+            prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+          />
+        </ItemForm>
 
-          <ItemForm>
-            <Button htmlType="submit" type="primary">
-              Login
-            </Button>
-          </ItemForm>
-        </Form>
+        <ItemForm>
+          <Button htmlType="submit" type="primary">
+            Login
+          </Button>
+        </ItemForm>
+      </Form>
     </div>
   );
 };
