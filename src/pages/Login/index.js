d3777329e2a7cceb32a2efce73969b2c3a55ae34
jessiLeandro <jessi_leandro@hotmail.com>
2020-12-04 11:19:28 -0300

    fix function handleSubmit, fix value at signIn function

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,58 +4,63 @@ import { has } from 'ramda';
 import { message } from 'antd';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { bindActionCreators } from "redux";
+import { bindActionCreators } from 'redux';
 
 import LoginContainer from '../../containers/Login';
 import { signIn } from '../../services/auth';
-import { onSubmit } from './LoginRedux/action'
+import { onSubmit } from './LoginRedux/action';
 
 class LoginPage extends Component {
   state = {
     authenticated: false,
-  }
+  };
 
   verifyAuth = () => {
-
-    if(has('auth', this.props) && has('token', this.props.auth)){
-      return this.setState({authenticated : uuidValidate(this.props.auth.token)})
+    if (has('auth', this.props) && has('token', this.props.auth)) {
+      return this.setState({ authenticated: uuidValidate(this.props.auth.token) });
     }
-  }
+  };
 
   success = () => {
     message.success('Bem-vindo ao Estoque');
   };
 
   error = () => {
-    message.error("Inautorizado, confira username e senha por favor");
+    message.error('Inautorizado, confira username e senha por favor');
   };
 
   handleSubmit = async ({ username, password }) => {
-    const { status, data } = await signIn({ username, password });
+    try {
+      const { status, data } = await signIn({
+        username,
+        password,
+        typeAccount: {
+          stock: true,
+        },
+      });
 
-    switch (status) {
-      case 200:
-        await this.props.onSubmit(data);
-        this.verifyAuth()
+      if (Number(status) === 401) {
+        throw new Error('User UNAUTHORIZED');
+      }
 
-        return this.state.authenticated && this.success();
-      case 401:
-
-        return this.error();
-      default:
+      await this.props.onSubmit(data);
+      this.verifyAuth();
+      return this.state.authenticated && this.success();
+    } catch (error) {
+      return this.error();
     }
   };
 
   componentDidMount = () => {
-    this.verifyAuth()
-  }
+    this.verifyAuth();
+  };
 
   render() {
-    return (
-      this.state.authenticated
-        ?  <Redirect to="/logged" />
-        :  <LoginContainer onSubmit={this.handleSubmit} />
-    )
+    return this.state.authenticated ? (
+      <Redirect to="/logged" />
+    ) : (
+      <LoginContainer onSubmit={this.handleSubmit} />
+    );
   }
 }
 
