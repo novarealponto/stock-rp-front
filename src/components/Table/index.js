7e1290af16354bf152e45afa14462f6a6a94ee0a
jessiLeandro <jessi_leandro@hotmail.com>
2020-12-09 16:39:00 -0300

    chore: update component Table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Table } from 'antd';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Table } from 'antd'
 
-const TableComponent = ({ columns, data }) => (
-  <Table columns={columns} dataSource={data} />
-);
+const TableComponent = ({ columns, data, pagination, onChange }) => (
+  <Table
+    columns={columns}
+    dataSource={data}
+    pagination={pagination}
+    onChange={onChange}
+  />
+)
 
 TableComponent.propTypes = {
   columns: PropTypes.array.isRequired,
   data: PropTypes.array.isRequired,
-};
+  pagination: PropTypes.object,
+  onChange: PropTypes.func,
+}
 
-export default TableComponent;
+export default TableComponent
