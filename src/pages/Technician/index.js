88712580f7ff1dd71943fb0e7e7e3fd54e86156e
jessiLeandro <jessi_leandro@hotmail.com>
2020-12-11 16:29:53 -0300

    chore: create route and navigate to manager page (SideBar)

--- src/pages/Technician/index.js
+++ src/pages/Technician/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import AddTechnician from './AddTechnician'
+import EditTechnician from './EditTechnician'
 import Manage from './Manage'
 
 const Technician = () => (
   <Switch>
     <Route exact path="/logged/technician/add" component={AddTechnician} />
+    <Route exact path="/logged/technician/edit" component={EditTechnician} />
     <Route exact path="/logged/technician/manager" component={Manage} />
   </Switch>
 )
