769d05e4fad67142e4c1e63f2bed1411d51a4179
jessiLeandro <jessi_leandro@hotmail.com>
2020-12-14 17:32:31 -0300

    chore: add title on containers

--- src/containers/ProductType/AddProductType/index.js
+++ src/containers/ProductType/AddProductType/index.js
@@ -1,33 +1,44 @@
 import React from 'react'
-import { Button, Col, Form, Input, Row } from 'antd'
+import { Button, Col, Form, Input, Row, Typography } from 'antd'
+
+const { Title } = Typography
 
 const AddProductType = ({ form, handleSubmit }) => {
   return (
-    <Form
-      form={form}
-      name="add productType"
-      onFinish={handleSubmit}
-      style={{ width: '100%' }}
-    >
-      <Row gutter={24} justify="space-between">
-        <Col flex="auto">
-          <Form.Item
-            label="Tipo de produto:"
-            name="productType"
-            rules={[{ required: true }]}
-          >
-            <Input placeholder="Digite o tipo de produto" />
-          </Form.Item>
-        </Col>
-        <Col flex="80px">
-          <Form.Item>
-            <Button htmlType="submit" type="primary">
-              Salvar
-            </Button>
-          </Form.Item>
-        </Col>
-      </Row>
-    </Form>
+    <Row>
+      <Col span={24}>
+        <Title>Tipo de produto</Title>
+      </Col>
+      <Col span={24}>
+        <Form
+          form={form}
+          name="add productType"
+          onFinish={handleSubmit}
+          style={{ width: '100%' }}
+        >
+          <Row gutter={24} justify="space-between">
+            <Col flex="auto">
+              <Form.Item
+                label="Tipo de produto:"
+                name="productType"
+                rules={[{ required: true }]}
+              >
+                <Input placeholder="Digite o tipo de produto" />
+              </Form.Item>
+            </Col>
+            <Col flex="80px">
+              <Form.Item>
+                <Button htmlType="submit"
+                //  type="primary"
+                 >
+                  Salvar
+                </Button>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </Col>
+    </Row>
   )
 }
 
--- src/containers/ProductType/UpdateProductType/index.js
+++ src/containers/ProductType/UpdateProductType/index.js
@@ -1,33 +1,42 @@
 import React from 'react'
-import { Button, Col, Form, Input, Row } from 'antd'
+import { Button, Col, Form, Input, Row, Typography } from 'antd'
+
+const { Title } = Typography
 
 const UpdateProductType = ({ form, handleSubmit, initialValues }) => {
   return (
-    <Form
-      form={form}
-      initialValues={initialValues}
-      name="add productType"
-      onFinish={handleSubmit}
-    >
-      <Row gutter={24} justify="space-between">
-        <Col flex="auto">
-          <Form.Item
-            label="Tipo de produto:"
-            name="productType"
-            rules={[{ required: true }]}
-          >
-            <Input placeholder="Digite o tipo de produto" />
-          </Form.Item>
-        </Col>
-        <Col flex="80px">
-          <Form.Item>
-            <Button htmlType="submit" type="primary">
-              Atualizar
-            </Button>
-          </Form.Item>
-        </Col>
-      </Row>
-    </Form>
+    <Row>
+      <Col span={24}>
+        <Title>Tipo de produto</Title>
+      </Col>
+      <Col span={24}>
+        <Form
+          form={form}
+          initialValues={initialValues}
+          name="add productType"
+          onFinish={handleSubmit}
+        >
+          <Row gutter={24} justify="space-between">
+            <Col flex="auto">
+              <Form.Item
+                label="Tipo de produto:"
+                name="productType"
+                rules={[{ required: true }]}
+              >
+                <Input placeholder="Digite o tipo de produto" />
+              </Form.Item>
+            </Col>
+            <Col flex="80px">
+              <Form.Item>
+                <Button htmlType="submit" type="primary">
+                  Atualizar
+                </Button>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </Col>
+    </Row>
   )
 }
 
