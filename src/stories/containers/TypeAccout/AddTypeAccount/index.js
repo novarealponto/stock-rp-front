2f3839969db5350d4fdbb8d16487866a1b1c1bbe
jessiLeandro <jessi_leandro@hotmail.com>
2020-12-15 13:53:24 -0300

    chore:  change container and storybook

--- src/stories/containers/TypeAccout/AddTypeAccount/index.js
+++ src/stories/containers/TypeAccout/AddTypeAccount/index.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { Form } from 'antd'
 
 import AddTypeAccountContainer from '../../../../containers/TypeAccount/AddTypeAccount'
-import PERMISSIONS from '../../../../utils/permissions';
+import buildTypeAccount from '../../../../utils/typeAccountSpec'
+import PERMISSIONS from '../../../../utils/permissions'
+
+const submitFormAction = action('Register new type account')
 
 export default {
   title: 'Containers/TypeAccount',
-  component: AddTypeAccountContainer
+  component: AddTypeAccountContainer,
 }
 
-const Template = (args) => <AddTypeAccountContainer permissions={PERMISSIONS} />
+const Template = (args) => {
+  const [form] = Form.useForm()
+
+  const handleSubmit = (typeAccountFormData) =>
+    submitFormAction(buildTypeAccount(typeAccountFormData))
+
+  return (
+    <AddTypeAccountContainer
+      form={form}
+      handleSubmit={handleSubmit}
+      permissions={PERMISSIONS}
+    />
+  )
+}
 
 export const AddTypeAccount = Template.bind({})
