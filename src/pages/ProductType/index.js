ba0e4a22cc62f05a9e38793ff17b3af9e751837a
jessiLeandro <jessi_leandro@hotmail.com>
2020-12-16 09:36:02 -0300

    chore: remove pages add and update

--- src/pages/ProductType/index.js
+++ src/pages/ProductType/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import AddProductType from './AddProductType'
 import Manager from './Manager'
 
 const Product = () => (
   <Switch>
-    <Route exact path="/logged/productType/add" component={AddProductType} />
     <Route exact path="/logged/productType/manager" component={Manager} />
   </Switch>
 )
