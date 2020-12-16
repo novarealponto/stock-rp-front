3c164424c19f40ade8cbd966412e4cf75a5465fc
jessiLeandro <jessi_leandro@hotmail.com>
2020-12-16 15:20:51 -0300

    chore: configuration routes

--- src/pages/TypeAcconunt/index.js
+++ src/pages/TypeAcconunt/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import AddTypeAcoount from './AddTypeAcoount'
+import Manager from './Manager'
 
 const User = () => (
   <Switch>
     <Route exact path='/logged/typeAccount/add' component={AddTypeAcoount} />
+    <Route exact path='/logged/typeAccount/manager' component={Manager} />
   </Switch>
 )
 
