dfba5561a6f6b43334171ed486b1bdfee9437c44
jessiLeandro <jessi_leandro@hotmail.com>
2020-12-17 15:17:22 -0300

    chore: create container

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -2,6 +2,7 @@ import { has, isEmpty, length, path } from 'ramda'
 import { isValid as cpnjIsValid } from '@fnando/cnpj'
 import { string, setLocale } from 'yup'
 
+import { getAddressByZipCode } from '../services/fornecedores'
 import { getSerial } from '../services/serialNumber'
 import { getAddressByZipCode } from '../services/fornecedores'
 import { getSerial } from '../services/serialNumber'
@@ -43,9 +44,48 @@ export const validateEmail = async (value) => {
   }
 }
 
-export const validatePlate = async (value) => {
-  if (/\W/.test(value)) {
-    return Promise.reject(new Error('Digite apenas caracteres alfanumérico'))
+setLocale({
+  mixed: {
+    default: 'Não é válido',
+  },
+  string: {
+    email: 'Email inválido',
+  },
+})
+
+export const validateCEP = async (form, value) => {
+  try {
+    const { status, data } = await getAddressByZipCode(value)
+
+    if (status !== 200 || has('erro', data)) {
+      return Promise.reject(new Error('Cep inválido'))
+    }
+
+    form.setFieldsValue({
+      city: path(['localidade'], data),
+      neighborhood: path(['bairro'], data),
+      referencePoint: path(['complemento'], data),
+      state: path(['uf'], data),
+      street: path(['logradouro'], data),
+    })
+  } catch (err) {
+    return Promise.reject(new Error('Cep inválido'))
+  }
+}
+
+export const validateCNPJ = (form, value) => {
+  if (!cpnjIsValid(value)) {
+    return Promise.reject(new Error('CNPJ inválido'))
+  } else {
+    return Promise.resolve(form.setFields([{ name: 'cnpj', value, errors: [] }]))
+  }
+}
+
+export const validateEmail = async (value) => {
+  try {
+    await string().email().validate(value)
+  } catch (err) {
+    return Promise.reject(err)
   }
 }
 
@@ -79,5 +119,11 @@ export const validateSerialNumberForEntry = async (
 
   if (data) {
     return setSerialNumberModal('Número de série já registrado!')
+  }
+}
+
+export const validatePlate = async (value) => {
+  if (/\W/.test(value)) {
+    return Promise.reject(new Error('Digite apenas caracteres alfanumérico'))
   }
 }
