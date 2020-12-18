fa6fc8e21f806669b31df8e451c51f05ea98f875
jessiLeandro <jessi_leandro@hotmail.com>
2020-12-18 16:35:01 -0300

    chore: create page and route

--- src/pages/Provider/index.js
+++ src/pages/Provider/index.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom'
 
 import AddProvider from './AddProvider'
 import Manager from './Manager'
+import UpdateProvider from './UpdateProvider'
 
 const Provider = () => (
   <Switch>
     <Route exact path="/logged/provider/add" component={AddProvider} />
     <Route exact path="/logged/provider/manager" component={Manager} />
+    <Route exact path="/logged/provider/update" component={UpdateProvider} />
   </Switch>
 )
 
