8bd3f57ee718e76a723aedfe9283786a65a65729
jessiLeandro <jessi_leandro@hotmail.com>
2020-12-21 17:36:19 -0300

    chore: create storybook

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,8 +1,9 @@
-import { has, path } from 'ramda'
+import { has, isEmpty, length, path } from 'ramda'
 import { isValid as cpnjIsValid } from '@fnando/cnpj'
 import { string, setLocale } from 'yup'
 
 import { getAddressByZipCode } from '../services/fornecedores'
+import { getSerial } from '../services/serialNumber'
 
 setLocale({
   mixed: {
@@ -52,5 +53,38 @@ export const validateEmail = async (value) => {
 export const validatePlate = async (value) => {
   if (/\W/.test(value)) {
     return Promise.reject(new Error('Digite apenas caracteres alfanumérico'))
+  }
+}
+
+export const validateSerialNumberForEntry = async (
+  currentTargetValue,
+  noRequest = false
+) => {
+  const currentValueSerialNumber = currentTargetValue.split(/\n/)
+  const lastPosition = length(currentValueSerialNumber) - 1
+
+  const findSerialNumber = currentValueSerialNumber.filter(
+    (serialNumber) => serialNumber === currentValueSerialNumber[lastPosition]
+  )
+
+  const setSerialNumberModal = (error) => {
+    currentValueSerialNumber.splice(lastPosition, 1)
+    return { serialNumbers: currentValueSerialNumber.join('\n'), error }
+  }
+
+  if (isEmpty(currentValueSerialNumber[lastPosition])) {
+    return setSerialNumberModal('Insída um número!')
+  }
+
+  if (findSerialNumber && length(findSerialNumber) > 1) {
+    return setSerialNumberModal('Número de série já foi adicionado!')
+  }
+
+  if (noRequest) return
+
+  const { data } = await getSerial(currentValueSerialNumber[lastPosition])
+
+  if (data) {
+    return setSerialNumberModal('Número de série já registrado!')
   }
 }
