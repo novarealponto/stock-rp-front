62b73cf930845d155dba487ff5df821612856700
jessiLeandro <jessi_leandro@hotmail.com>
2020-12-28 10:55:59 -0300

    chore: changa utils

--- src/utils/filterData.js
+++ src/utils/filterData.js
@@ -5,11 +5,14 @@ const applyMatch = (key, object, query) =>
 
 const createRegex = (pattern) => new RegExp(`${pattern}`, 'gi')
 
-const filterData = (query, initialData) => {
+const filterData = (query, initialData, options = { and: false }) => {
+  const { and } = options
   const callbackFilterInitialData = (item) => {
     const callbackFilterKeys = (key) => length(applyMatch(key, item, query)) > 0
 
-    return length(filter(callbackFilterKeys, keys(query))) > 0
+    const lengthFilter = length(filter(callbackFilterKeys, keys(query)))
+
+    return and ? lengthFilter === length(keys(query)) : lengthFilter > 0
   }
   return filter(callbackFilterInitialData, initialData)
 }
