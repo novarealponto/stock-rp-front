4618f82050079ca17c2200de55cd5626b97acba4
jessiLeandro <jessi_leandro@hotmail.com>
2021-01-06 10:57:50 -0300

    chore: create container

--- src/containers/OS/AddOS/index.js
+++ src/containers/OS/AddOS/index.js
@@ -1,37 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import moment from 'moment'
+import styles from './style.module.css'
 import {
   Button,
   Col,
   DatePicker,
   Divider,
-  Input,
   Form,
+  Input,
+  InputNumber,
   Row,
   Select,
-  Space,
-  InputNumber,
   Typography,
 } from 'antd'
 import { map } from 'ramda'
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
 
-const { Title } = Typography
+import masks from '../../../utils/masks'
+import { validateCNPJ } from '../../../utils/validators'
+
 const { Option } = Select
 const { TextArea } = Input
+const { Title } = Typography
 
-const sights = {
-  Beijing: ['Tiananmen', 'Great Wall'],
-  Shanghai: ['Oriental Pearl', 'The Bund'],
-}
+const rules = [{ required: true }]
 
 const Manager = ({
-  technicianList,
-  statusList,
+  form,
+  handleSubmit,
+  onChangeStatus,
+  onPressEnterTextAreaSerialNumber,
   productList,
-  visibleTextArea,
-  handleChangeProduct,
+  status,
+  statusList,
+  technicianList,
 }) => {
-  const [form] = Form.useForm()
+  const [max, setMax] = useState(1)
+  const [productBaseId, setProductBaseId] = useState('')
+  const [visibleTextArea, setVisibleTextArea] = useState(false)
+
+  const handleChangeProduct = (_, { key, max, serial }) => {
+    setMax(max)
+    setProductBaseId(key)
+    setVisibleTextArea(serial)
+  }
+
   return (
     <>
       <Row justify="center">
@@ -41,34 +54,53 @@ const Manager = ({
       </Row>
 
       <Form
-        layout="vertical"
         form={form}
         initialValues={{ quantity: 1 }}
-        onFinish={console.log}
+        layout="vertical"
+        onFinish={(formData) => {
+          handleSubmit(formData)
+          setMax(1)
+          setProductBaseId('')
+          setVisibleTextArea(false)
+        }}
+        validateTrigger="onBlur"
       >
         <Row>
           <Col span={24}>
-            <Form.Item label="Razão Social">
+            <Form.Item label="Razão Social" name="razaoSocial" rules={rules}>
               <Input placeholder="Digite a razão social" />
             </Form.Item>
           </Col>
         </Row>
         <Row gutter={10}>
           <Col span={8}>
-            <Form.Item label="CNPJ">
-              <Input placeholder="Digite o CNPJ" />
-            </Form.Item>
-          </Col>
-          <Col span={8}>
-            <Form.Item label="Data de atd.">
-              <DatePicker
-                style={{ width: '100%' }}
-                placeholder="Selecione a Data"
+            <Form.Item
+              label="CNPJ"
+              name="cnpj"
+              rules={[
+                { required: true },
+                { validator: (_, value) => validateCNPJ(form, value) },
+              ]}
+            >
+              <Input
+                onChange={({ target: { value } }) => masks(form, 'cnpj', value)}
+                placeholder="Digite o CNPJ"
               />
             </Form.Item>
           </Col>
           <Col span={8}>
-            <Form.Item label="Técnico">
+            <Form.Item label="Data de atd." name="date" rules={rules}>
+              <DatePicker
+                disabledDate={(current) =>
+                  current && current < moment().subtract(1, 'day')
+                }
+                placeholder="Selecione a Data"
+                style={{ width: '100%' }}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item label="Técnico" name="technician" rules={rules}>
               <Select placeholder="Selecione o Técnico">
                 {map(
                   ({ key, name }) => (
@@ -94,10 +126,10 @@ const Manager = ({
         <Row gutter={20}>
           <Col span={8}>
             <Form.Item label="Status" name="status">
-              <Select placeholder="Selecione/digite um status">
+              <Select onChange={onChangeStatus} placeholder="Selecione um status">
                 {map(
                   ({ key, status }) => (
-                    <Option key={key} value={key}>
+                    <Option key={key} value={status}>
                       {status}
                     </Option>
                   ),
@@ -107,14 +139,19 @@ const Manager = ({
             </Form.Item>
           </Col>
           <Col span={14}>
-            <Form.Item name="product" label="Nome do produto">
+            <Form.Item label="Nome do produto" name="product">
               <Select
-                placeholder="selecione um produto"
                 onChange={handleChangeProduct}
+                placeholder="selecione um produto"
               >
                 {map(
-                  ({ key, name, serial }) => (
-                    <Option key={key} value={name} serial={serial}>
+                  ({ category, key, max, name, serial }) => (
+                    <Option
+                      key={key}
+                      max={max}
+                      serial={category === 'equipamento' ? serial : undefined}
+                      value={name}
+                    >
                       {name}
                     </Option>
                   ),
@@ -125,19 +162,34 @@ const Manager = ({
           </Col>
           <Col span={2}>
             <Form.Item label="Qtd." name="quantity">
-              <InputNumber min={1} style={{ width: '100%' }} />
+              <InputNumber max={max} min={1} style={{ width: '100%' }} />
             </Form.Item>
           </Col>
         </Row>
 
-        {visibleTextArea && (
+        {(visibleTextArea || status === 'CONSERTO') && (
           <Form.Item label="Números de série" name="serialNumbers">
-            <TextArea rows={4} />
+            <TextArea
+              onPressEnter={(event) =>
+                onPressEnterTextAreaSerialNumber({ ...event, max })
+              }
+              rows={3}
+            />
           </Form.Item>
         )}
-
-        <Form.List name="products">
-          {(fields, { add, remove }) => (
+        <Form.List
+          name="products"
+          rules={[
+            {
+              validator: async (_, names) => {
+                if (!names || names.length < 1) {
+                  return Promise.reject(new Error('Insira ao menos um item'))
+                }
+              },
+            },
+          ]}
+        >
+          {(fields, { add, remove }, { errors }) => (
             <>
               <Row justify="end">
                 <Col>
@@ -151,36 +203,40 @@ const Manager = ({
                   >
                     {({ getFieldValue }) => {
                       return (
-                        !!getFieldValue('product') &&
-                        !!getFieldValue('status') && (
-                          <Form.Item>
-                            <Button
-                              type="primary"
-                              onClick={() => {
-                                add(
-                                  {
-                                    product: form.getFieldValue('product'),
-                                    status: form.getFieldValue('status'),
-                                    quantity: form.getFieldValue('quantity'),
-                                    serialNumbers: form.getFieldValue(
-                                      'serialNumbers'
-                                    ),
-                                  },
-                                  0
-                                )
-                                form.setFieldsValue({
-                                  product: undefined,
-                                  quantity: 1,
-                                  serialNumbers: undefined,
-                                  status: undefined,
-                                })
-                              }}
-                              icon={<PlusOutlined />}
-                            >
-                              Adicionar
-                            </Button>
-                          </Form.Item>
-                        )
+                        <Form.Item>
+                          <Button
+                            icon={<PlusOutlined />}
+                            disabled={
+                              !getFieldValue('product') ||
+                              !getFieldValue('status')
+                            }
+                            style={{ width: '155px' }}
+                            onClick={() => {
+                              add(
+                                {
+                                  product: form.getFieldValue('product'),
+                                  productBaseId,
+                                  status: form.getFieldValue('status'),
+                                  quantity: form.getFieldValue('quantity'),
+                                  serialNumbers: form.getFieldValue(
+                                    'serialNumbers'
+                                  ),
+                                },
+                                0
+                              )
+                              form.setFieldsValue({
+                                product: undefined,
+                                quantity: 1,
+                                serialNumbers: undefined,
+                                status: undefined,
+                              })
+                            }}
+                            type="primary"
+                          >
+                            Adicionar
+                          </Button>
+                          <Form.ErrorList errors={errors} />
+                        </Form.Item>
                       )
                     }}
                   </Form.Item>
@@ -189,26 +245,24 @@ const Manager = ({
 
               <Divider />
 
-              {fields.map((field, index) => (
-                <Form.Item required={false} key={field.key}>
+              {fields.map((field) => (
+                <Form.Item key={field.key} required={false}>
                   <Row>
                     <Col flex="auto">
                       <Row gutter={10}>
-                        <Col span={20}>
+                        <Col span={21}>
                           <Form.Item
-                            {...field}
-                            name={[field.name, 'product']}
                             fieldKey={[field.fieldKey, 'product']}
+                            name={[field.name, 'product']}
                             noStyle
                           >
                             <Input placeholder="produto" readOnly />
                           </Form.Item>
                         </Col>
-                        <Col span={4}>
+                        <Col span={3}>
                           <Form.Item
-                            {...field}
-                            name={[field.name, 'quantity']}
                             fieldKey={[field.fieldKey, 'quantity']}
+                            name={[field.name, 'quantity']}
                             noStyle
                           >
                             <InputNumber placeholder="quantidade" readOnly />
@@ -218,34 +272,26 @@ const Manager = ({
                     </Col>
                     <Col flex="50px">
                       <MinusCircleOutlined
-                        className="dynamic-delete-button"
+                        className={styles.deleteButton}
                         onClick={() => remove(field.name)}
                       />
                     </Col>
                   </Row>
-                  <Form.Item
-                    {...field}
-                    name={[field.name, 'serialNumbers']}
-                    fieldKey={[field.fieldKey, 'serialNumbers']}
-                    noStyle
-                  />
-                  <Form.Item
-                    {...field}
-                    name={[field.name, 'status']}
-                    fieldKey={[field.fieldKey, 'status']}
-                    noStyle
-                  />
                 </Form.Item>
               ))}
             </>
           )}
         </Form.List>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Send
-          </Button>
-        </Form.Item>
+        <Row justify="end">
+          <Col>
+            <Form.Item>
+              <Button htmlType="submit" type="primary">
+                Salvar
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
       </Form>
     </>
   )
