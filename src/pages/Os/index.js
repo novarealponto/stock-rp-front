706addc0ebc0c34bfc3ec6d285bf61a0d4ed6723
jessiLeandro <jessi_leandro@hotmail.com>
2021-01-07 15:26:31 -0300

    chore: create page and route

--- src/pages/Os/index.js
+++ src/pages/Os/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import AddOs from './AddOs'
+import UpdateOS from './UpdateOS'
 import Manager from './Manager'
 
 const Os = () => (
   <Switch>
     <Route exact path="/logged/os/add" component={AddOs} />
     <Route exact path="/logged/os/manager" component={Manager} />
+    <Route exact path="/logged/os/update" component={UpdateOS} />
   </Switch>
 )
 
