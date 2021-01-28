5450eff9b97fa016e1ace8101ef5adfd61e706e5
jessiLeandro <jessi_leandro@hotmail.com>
2021-01-28 15:35:57 -0300

    chore: create storybook ReportOS

---
+++ src/stories/containers/Report/Os/index.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import moment from 'moment'
+import { commerce, company, date, name, random } from 'faker'
+
+import ReportOSContainer from '../../../../containers/Report/Os'
+
+export default {
+  title: 'Containers/Repots',
+  component: ReportOSContainer,
+}
+
+const osList = []
+const statusList = []
+const technicianList = []
+
+for (let key = 0; key < 20; key++) {
+  osList.push({
+    date: moment(date.past()).format('L'),
+    key,
+    os: key,
+    products: [
+      {
+        missout: random.number({ max: 10, precision: 1 }),
+        output: random.number({ max: 10, precision: 1 }),
+        product: commerce.productName(),
+        reserve: random.number({ max: 10, precision: 1 }),
+        return: random.number({ max: 10, precision: 1 }),
+        status: 'VENDA',
+      },
+    ],
+    razaoSocial: company.companyName(),
+    technician: name.firstName(),
+  })
+
+  statusList.push({
+    id: key,
+    status: commerce.department(),
+  })
+
+  technicianList.push({
+    id: key,
+    name: name.firstName(),
+  })
+}
+
+const Template = (args) => {
+  return <ReportOSContainer {...args} />
+}
+
+export const Os = Template.bind({})
+
+Os.args = {
+  osList,
+  statusList,
+  technicianList,
+}
