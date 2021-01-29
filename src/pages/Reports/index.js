399c1eb329ceeaa3d77c2c89282ab9081a05663f
jessiLeandro <jessi_leandro@hotmail.com>
2021-01-29 09:20:52 -0300

    chore: create page Solds

--- src/pages/Reports/index.js
+++ src/pages/Reports/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import ReportOS from './Os'
 import ReportMissout from './Missout'
+import ReportOS from './Os'
+import ReportSolds from './Solds'
 
 const ReportsRoutes = () => (
   <Switch>
-    <Route component={ReportOS} exact path="/logged/reports/os" />
     <Route component={ReportMissout} exact path="/logged/reports/missout" />
+    <Route component={ReportOS} exact path="/logged/reports/os" />
+    <Route component={ReportSolds} exact path="/logged/reports/solds" />
   </Switch>
 )
 
