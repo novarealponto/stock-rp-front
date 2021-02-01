543463bef12574bb4ab576d5ab132378b2a30120
Guilherme Stain <guilhermestain@localhost.localdomain>
2021-02-01 17:21:07 -0300

    chore: fix e-commerce and pattern

--- src/containers/E-Commerce/Add/index.js
+++ src/containers/E-Commerce/Add/index.js
@@ -72,7 +72,7 @@ const ECommerce = ({
     <>
       <Row gutter={[0, 20]} justify="center">
         <Col>
-          <Title level={3}>Reserva E-Commerce</Title>
+          <Title level={3}>Reserva E-commerce</Title>
         </Col>
       </Row>
 
@@ -99,7 +99,7 @@ const ECommerce = ({
               name="name"
               rules={[{ required: true }]}
             >
-              <Input placeholder="Digite o nome ou a razao social" />
+              <Input placeholder="Digite o nome ou a razão social" />
             </Form.Item>
           </Col>
           <Col span={6}>
