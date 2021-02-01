bf77a3df8174316d626e110b1c56901e62a23aab
Guilherme Stain <guilhermestain@localhost.localdomain>
2021-02-01 16:47:18 -0300

    chore: fix manager user and pattern

--- src/containers/User/Manager/index.js
+++ src/containers/User/Manager/index.js
@@ -1,33 +1,25 @@
-import React from 'react';
-import {
-  Button,
-  Col,
-  Input,
-  Row,
-  Table,
-  Tooltip,
-  Typography,
-} from 'antd';
+import React from 'react'
+import { Button, Col, Input, Row, Table, Tooltip, Typography } from 'antd'
 import {
   EditOutlined,
   CheckCircleTwoTone,
   CloseCircleTwoTone,
-} from '@ant-design/icons';
-import { always, ifElse, pathEq } from 'ramda';
-import PropTypes from 'prop-types';
+} from '@ant-design/icons'
+import { always, ifElse, pathEq } from 'ramda'
+import PropTypes from 'prop-types'
 
-import styles from './style.module.css';
+import styles from './style.module.css'
 
-const { Search } = Input;
-const { Title } = Typography;
+const { Search } = Input
+const { Title } = Typography
 
 const customizedIcon = ifElse(
   pathEq(['customized'], true),
   always(<CheckCircleTwoTone twoToneColor="#52c41a" />),
-  always(<CloseCircleTwoTone twoToneColor="#f01b0c" />),
-);
+  always(<CloseCircleTwoTone twoToneColor="#f01b0c" />)
+)
 
-const updateUserButton = goToUpdateUser => (_, user) => (
+const updateUserButton = (goToUpdateUser) => (_, user) => (
   <Tooltip placement="topLeft" title="Editar">
     <EditOutlined
       className="icon-edit"
@@ -36,25 +28,27 @@ const updateUserButton = goToUpdateUser => (_, user) => (
       theme="outlined"
     />
   </Tooltip>
-);
+)
 
-const columns = goToUpdateUser => ([{
-  dataIndex: 'username',
-  title: 'Usuário',
-},
-{
-  dataIndex: 'typeName',
-  title: 'Tipo de conta',
-},
-{
-  dataIndex: 'customized',
-  render: customized => customizedIcon({ customized }),
-  title: 'Customizado',
-},
-{
-  render: updateUserButton(goToUpdateUser),
-  title: '',
-}]);
+const columns = (goToUpdateUser) => [
+  {
+    dataIndex: 'username',
+    title: 'Usuário',
+  },
+  {
+    dataIndex: 'typeName',
+    title: 'Tipo de conta',
+  },
+  {
+    dataIndex: 'customized',
+    render: (customized) => customizedIcon({ customized }),
+    title: 'Customizado',
+  },
+  {
+    render: updateUserButton(goToUpdateUser),
+    title: '',
+  },
+]
 
 const Manager = ({
   data,
@@ -66,11 +60,13 @@ const Manager = ({
 }) => {
   return (
     <div className={styles.managerUserContainer}>
-      <Row gutter={[8, 16]}>
-        <Col span={24}>
+      <Row justify="center">
+        <Col>
           <Title level={3}>Gerenciar usuários</Title>
         </Col>
-        <Col span={16} md={{ span: 18 }}>
+      </Row>
+      <Row gutter={[8, 18]}>
+        <Col span={18} md={{ span: 18 }}>
           <Search
             placeholder="Filtre pelo nome do usuário"
             onSearch={handleSearch}
@@ -79,8 +75,8 @@ const Manager = ({
           />
         </Col>
         <Col span={8} md={{ span: 6 }}>
-          <Button type="primary" onClick={goToAddUser}>
-            Adicionar Usuário
+          <Button onClick={goToAddUser}>
+            Adicionar novo usuário
           </Button>
         </Col>
         <Col span={24}>
@@ -93,8 +89,8 @@ const Manager = ({
         </Col>
       </Row>
     </div>
-  );
-};
+  )
+}
 
 Manager.propTypes = {
   data: PropTypes.arrayOf(
@@ -102,13 +98,13 @@ Manager.propTypes = {
       username: PropTypes.string.isRequired,
       typeName: PropTypes.string.isRequired,
       customized: PropTypes.bool.isRequired,
-    }).isRequired,
+    }).isRequired
   ),
   goToAddUser: PropTypes.func.isRequired,
   goToUpdateUser: PropTypes.func.isRequired,
   handlePaginations: PropTypes.func.isRequired,
   handleSearch: PropTypes.func.isRequired,
   loading: PropTypes.bool.isRequired,
-};
+}
 
-export default Manager;
+export default Manager
