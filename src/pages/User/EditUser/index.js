72c3c996ec85f2eecb454cf4aef0f75b5a097519
Guilherme Stain <guilhermestain@localhost.localdomain>
2021-02-09 16:00:51 -0300

    chore: fix conflicts

--- src/pages/User/EditUser/index.js
+++ src/pages/User/EditUser/index.js
@@ -1,15 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { withRouter } from 'react-router-dom'
-<<<<<<< HEAD
 import { Form, message } from 'antd'
 import { compose, length, path, pathOr, pipe } from 'ramda'
-=======
-import { connect } from 'react-redux'
-import { Form, message } from 'antd'
-import { compose, path } from 'ramda'
-import PropTypes from 'prop-types'
-import { bindActionCreators } from 'redux'
->>>>>>> 7c53950... chore: all changes
 
 import {
   getResourcesByTypeAccount,
@@ -20,10 +12,6 @@ import { getTypeAccount } from '../../../services/typeAccount'
 import { buildUser, buildUserUpdate } from '../../../utils/userSpec'
 import EditUserContainer from '../../../containers/User/EditUser'
 import PERMISSIONS from '../../../utils/permissions'
-<<<<<<< HEAD
-=======
-import { clearValueUsuario } from '../../../store/Actions/user'
->>>>>>> 7c53950... chore: all changes
 
 const createUserText = 'Usuário alterado com sucesso!'
 const unableCreateUserText = 'Não foi possível alterar o usuário!'
@@ -33,11 +21,7 @@ const unableSetThatAccountType =
 const successMessage = (messageText) => message.success(messageText)
 const errorMessage = (messageText) => message.error(messageText)
 
-<<<<<<< HEAD
 const EditUser = () => {
-=======
-const EditUser = ({ clearValueUsuario, userReducer }) => {
->>>>>>> 7c53950... chore: all changes
   const [allowCustomPermissions, setAllowCustomPermissions] = useState(false)
   const [form] = Form.useForm()
   const [typeAccounts, setTypeAccounts] = useState([])
@@ -114,7 +98,6 @@ const EditUser = ({ clearValueUsuario, userReducer }) => {
   }
 
   useEffect(() => {
-<<<<<<< HEAD
     const pathname = pipe(pathOr('', ['location', 'pathname']), (item) =>
       item.split('/')
     )(window)
@@ -122,21 +105,12 @@ const EditUser = ({ clearValueUsuario, userReducer }) => {
     const id = pathname[length(pathname) - 1]
 
     getUserById(id).then(({ data }) => {
-=======
-    getUserById(userReducer.id).then(({ data }) => {
->>>>>>> 7c53950... chore: all changes
       form.setFieldsValue(buildUserUpdate(data))
       setAllowCustomPermissions(!!path(['customized'], data))
     })
 
     getAllTypeAccount()
-<<<<<<< HEAD
   }, [form, getAllTypeAccount])
-=======
-
-    return () => clearValueUsuario()
-  }, [form, userReducer, getAllTypeAccount])
->>>>>>> 7c53950... chore: all changes
 
   return (
     <EditUserContainer
@@ -151,56 +125,6 @@ const EditUser = ({ clearValueUsuario, userReducer }) => {
   )
 }
 
-<<<<<<< HEAD
 const enhanced = compose(withRouter)
-=======
-const mapStateToProps = ({ userReducer }) => ({
-  userReducer,
-})
-
-const mapDispacthToProps = (dispach) =>
-  bindActionCreators({ clearValueUsuario }, dispach)
-
-const enhanced = compose(connect(mapStateToProps, mapDispacthToProps), withRouter)
-
-EditUser.propTypes = {
-  userReducer: PropTypes.shape({
-    customized: PropTypes.bool.isRequired,
-    id: PropTypes.string.isRequired,
-    resource: PropTypes.shape({
-      addAccessories: PropTypes.bool.isRequired,
-      addAnalyze: PropTypes.bool.isRequired,
-      addCar: PropTypes.bool.isRequired,
-      addCompany: PropTypes.bool.isRequired,
-      addEntr: PropTypes.bool.isRequired,
-      addEntry: PropTypes.bool.isRequired,
-      addEquip: PropTypes.bool.isRequired,
-      addEquipType: PropTypes.bool.isRequired,
-      addFonr: PropTypes.bool.isRequired,
-      addKit: PropTypes.bool.isRequired,
-      addKitOut: PropTypes.bool.isRequired,
-      addMark: PropTypes.bool.isRequired,
-      addOutPut: PropTypes.bool.isRequired,
-      addPart: PropTypes.bool.isRequired,
-      addProd: PropTypes.bool.isRequired,
-      addRML: PropTypes.bool.isRequired,
-      addROs: PropTypes.bool.isRequired,
-      addStatus: PropTypes.bool.isRequired,
-      addTec: PropTypes.bool.isRequired,
-      addType: PropTypes.bool.isRequired,
-      addTypeAccount: PropTypes.bool.isRequired,
-      addUser: PropTypes.bool.isRequired,
-      delROs: PropTypes.bool.isRequired,
-      gerROs: PropTypes.bool.isRequired,
-      modulo: PropTypes.bool.isRequired,
-      suprimento: PropTypes.bool.isRequired,
-      tecnico: PropTypes.bool.isRequired,
-      updateRos: PropTypes.bool.isRequired,
-    }).isRequired,
-    typeName: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired,
-  }).isRequired,
-}
->>>>>>> 7c53950... chore: all changes
 
 export default enhanced(EditUser)
